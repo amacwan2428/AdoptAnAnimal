db28286a25e505499bd9e67c141cd3da72188541
Ruy "Lorde Supremo " Oliveira <ruygsgoliveira@live.com>
2022-12-06 18:09:46 -0500

    Added pet class and adapter

---
+++ app/src/main/java/com/example/adoptananimal/Pet.java
@@ -0,0 +1,47 @@
+package com.example.adoptananimal;
+
+public class Pet {
+    private int id;
+    private int userId;
+    private String name, birthdate,type;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
